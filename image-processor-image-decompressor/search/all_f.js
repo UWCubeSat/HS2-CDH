d7bec1b65c73d351dee72594ec81013d7c2de80c
MahirEmran <memran@cs.washington.edu>
2026-04-14 06:40:58 +0000

    Deploying to documentation from @ UWCubeSat/HS2-CDH@2a136ad1ee0ef709a3605d5ec97387f8fce25e99 🚀

--- image-processor-image-decompressor/search/all_f.js
+++ image-processor-image-decompressor/search/all_f.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['s_5fbitstreambuf_0',['s_bitstreamBuf',['../classImageProcessor_1_1ImageDecompressor.html#a5e6505964a9743a27b2a7f3ca4882186',1,'ImageProcessor::ImageDecompressor']]],
-  ['s_5fimagesamplebuf_1',['s_imageSampleBuf',['../classImageProcessor_1_1ImageCompressor.html#a2285d20a3b77f3cd783093f392d1a856',1,'ImageProcessor::ImageCompressor']]],
+  ['s_5fimagesamplebuf_1',['s_imageSampleBuf',['../classImageProcessor_1_1ImageCompressor.html#aa53a1073dfeb99d5143cd24337ddc9cf',1,'ImageProcessor::ImageCompressor']]],
   ['sequence_20diagrams_2',['sequence diagrams',['../md_FlightComputer_2Components_2EventScheduler_2docs_2sdd.html#autotoc_md7',1,'Sequence Diagrams'],['../md_FlightComputer_2Components_2ImageProcessor_2ImageCompressor_2docs_2sdd.html#autotoc_md22',1,'Sequence Diagrams'],['../md_FlightComputer_2Components_2ImageProcessor_2ImageDecompressor_2docs_2sdd.html#autotoc_md37',1,'Sequence Diagrams'],['../md_FlightComputer_2Components_2TmtcRadioManager_2docs_2sdd.html#autotoc_md51',1,'Sequence Diagrams']]],
   ['started_3',['Getting Started',['../index.html#autotoc_md62',1,'']]],
   ['states_4',['states',['../md_FlightComputer_2Components_2EventScheduler_2docs_2sdd.html#autotoc_md6',1,'Component States'],['../md_FlightComputer_2Components_2ImageProcessor_2ImageCompressor_2docs_2sdd.html#autotoc_md21',1,'Component States'],['../md_FlightComputer_2Components_2ImageProcessor_2ImageDecompressor_2docs_2sdd.html#autotoc_md36',1,'Component States'],['../md_FlightComputer_2Components_2TmtcRadioManager_2docs_2sdd.html#autotoc_md50',1,'Component States']]],
--- image-processor-image-decompressor/search/variables_2.js
+++ image-processor-image-decompressor/search/variables_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['s_5fbitstreambuf_0',['s_bitstreamBuf',['../classImageProcessor_1_1ImageDecompressor.html#a5e6505964a9743a27b2a7f3ca4882186',1,'ImageProcessor::ImageDecompressor']]],
-  ['s_5fimagesamplebuf_1',['s_imageSampleBuf',['../classImageProcessor_1_1ImageCompressor.html#a2285d20a3b77f3cd783093f392d1a856',1,'ImageProcessor::ImageCompressor']]]
+  ['s_5fimagesamplebuf_1',['s_imageSampleBuf',['../classImageProcessor_1_1ImageCompressor.html#aa53a1073dfeb99d5143cd24337ddc9cf',1,'ImageProcessor::ImageCompressor']]]
 ];
